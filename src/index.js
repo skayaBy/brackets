9fcad653b51e6358bfd67158be04f13631df0c1d
Zavadskaya Olga <zavadskaya.ov@gmail.com>
2023-02-12 19:19:32 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = [];
+  let closeBrackets = [];
+
+  for(let i = 0; i < bracketsConfig.length; i++){
+    openBrackets.push(bracketsConfig[i][0]);
+    closeBrackets.push(bracketsConfig[i][1]);
+  }
+
+  let stack = []; 
+  for(let i = 0; i < str.length; i++){
+    let currentSymbol = str[i];
+    let topElement = stack[stack.length-1];
+    
+    if(closeBrackets.includes(currentSymbol) && stack.length > 0 && closeBrackets.indexOf(currentSymbol) === openBrackets.indexOf(topElement)){
+      stack.pop();
+    }
+    else if(openBrackets.includes(currentSymbol)){
+      stack.push(currentSymbol);
+    } 
+    else {
+      return false;
+      }   
+    }
+
+  return stack.length === 0; 
 }
